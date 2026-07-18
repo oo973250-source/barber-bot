89a4cd3bf1cf7863c130740f103d9912b0763977
oo973250-source <oo973250@gmail.com>
2026-07-18 18:14:00 +0300

    Use environment variable for server port

--- chapa_server.py
+++ chapa_server.py
@@ -259,6 +259,5 @@ def confirm_booking():
 
     return jsonify({"status": "success"})
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, use_reloader=False)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), use_reloader=False)
